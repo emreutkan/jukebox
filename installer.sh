94758f55b282dc2953ffedae68eeddbfb81dc69d
emreutkan <127414322+emreutkan@users.noreply.github.com.>
2024-03-13 13:30:46 -0400

    Version 2 Installer

--- installer.sh
+++ installer.sh
@@ -3,20 +3,27 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+REPO_URL="https://github.com/emreutkan/jukebox.git"
+INSTALL_DIR="$HOME"
 
-# Check if running as root
-if [ "$EUID" -ne 0 ]
-  then echo -e "${RED}run as root (sudo ./installer.sh)${NC}"
-  exit
+if [ "$EUID" -ne 0 ]; then
+    echo -e "${RED}Please run as root (sudo ./installer.sh)${NC}"
+    exit
 fi
+
+cd $INSTALL_DIR
+echo -e "${GREEN}Cloning the jukebox repository to the root directory.${NC}"
+git clone "$REPO_URL"
+cd jukebox || exit
 
 if command -v pacman &> /dev/null; then
     PACKAGE_MANAGER_INSTALL="sudo pacman -S --noconfirm"
     PACKAGE_MANAGER_UPDATE="sudo pacman -Syu"
-  echo -e "${GREEN} Arch Based Distro detected.${NC}"
+    echo -e "${GREEN}Arch Based Distro detected.${NC}"
 elif command -v apt-get &> /dev/null; then
     PACKAGE_MANAGER_INSTALL="sudo apt-get install -y"
-    echo -e "${GREEN} Debian Based Distro detected.${NC}"
+    PACKAGE_MANAGER_UPDATE="sudo apt-get update"
+    echo -e "${GREEN}Debian Based Distro detected.${NC}"
 else
     echo -e "${RED}Supported package manager not found. Install packages manually.${NC}"
     exit 1
@@ -25,69 +32,35 @@ fi
 echo -e "${GREEN}Updating package manager${NC}"
 $PACKAGE_MANAGER_UPDATE
 
-if ! command -v aircrack-ng &> /dev/null
-then
-    echo -e "${GREEN}Installing aircrack-ng${NC}"
-    $PACKAGE_MANAGER_INSTALL aircrack-ng
-else
-    echo -e "${GREEN}aircrack-ng already installed.${NC}"
-fi
+DEPENDENCIES="aircrack-ng airgraph-ng net-tools python3 python3-pip git"
+for dep in $DEPENDENCIES; do
+    if ! command -v $dep &> /dev/null; then
+        echo -e "${GREEN}Installing $dep${NC}"
+        $PACKAGE_MANAGER_INSTALL $dep
+    else
+        echo -e "${GREEN}$dep already installed.${NC}"
+    fi
+done
 
-if ! command -v airgraph-ng &> /dev/null
-then
-    echo -e "${GREEN}Installing airgraph-ng${NC}"
-    $PACKAGE_MANAGER_INSTALL airgraph-ng
-else
-    echo -e "${GREEN}airgraph-ng already installed.${NC}"
-fi
-
-if ! command -v ifconfig &> /dev/null
-then
-    echo -e "${GREEN}Installing net-tools${NC}"
-    $PACKAGE_MANAGER_INSTALL net-tools
-else
-    echo -e "${GREEN}ifconfig already installed.${NC}"
-fi
-
-if ! command -v python3 &> /dev/null
-then
-    echo -e "${GREEN}Installing python3${NC}"
-    $PACKAGE_MANAGER_INSTALL python3
-fi
-
-if ! command -v pip3 &> /dev/null
-then
-    echo -e "${GREEN}Installing pip3${NC}"
-    $PACKAGE_MANAGER_INSTALL python3-pip
-fi
-
-chmod +x main.py
-
-# Create a virtual environment if it doesn't exist
 if [ ! -d "venv" ]; then
     echo -e "${GREEN}Creating a virtual environment${NC}"
     python3 -m venv venv
 fi
 
-
 echo -e "${GREEN}Activating the virtual environment and installing Python dependencies${NC}"
 source venv/bin/activate
 pip install -r requirements.txt
 
-
-# Create a jukebox command
 echo -e "${GREEN}Creating a jukebox command...${NC}"
 echo "#!/bin/bash
-# Check if running as root
 if [ \"\$EUID\" -ne 0 ]; then
-  echo 'run as root (sudo jukebox)'
-  exit
+    echo 'Please run as root (sudo jukebox)'
+    exit
 fi
-cd /path/to/your/project
+cd $INSTALL_DIR/jukebox
 source venv/bin/activate
-python main.py" > jukebox
+python jukebox.py" > jukebox
 
-# Make the jukebox command executable and move it to the user's bin directory
 chmod +x jukebox
 sudo mv jukebox /usr/local/bin/jukebox
 
